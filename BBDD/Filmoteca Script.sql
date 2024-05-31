00870d0912c4eb69687bc6e63b9b41e15525c6e6
PabloGonz68 <pablogonzalezsilva6@gmail.com>
2024-05-31 13:46:22 +0200

    [ADD] ComentarioExplicativo

--- BBDD/Filmoteca Script.sql
+++ BBDD/Filmoteca Script.sql
@@ -2,3 +2,4 @@ DROP DATABASE IF EXISTS filmoteca;
 
 CREATE DATABASE filmoteca;
 USE filmoteca;
+--Esto tienes que ponerlo en tu MySQL para poder crear y usar la base de datos ny ya depues ejecutar y utilizar el programa
